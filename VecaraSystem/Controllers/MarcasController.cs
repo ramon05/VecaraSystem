e53b6c83d4ac0c8c7baf5a940302dd36db8c7126
ramon05 <ramoncastillo051991@gmail.com>
2019-11-18 22:05:41 -0400

    Se agregó el controller Marcas

---
+++ VecaraSystem/Controllers/MarcasController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VecaraSystem.Data;
+using VecaraSystem.Models;
+
+namespace VecaraSystem.Controllers
+{
+    public class MarcasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MarcasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Marcas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Marcas.ToListAsync());
+        }
+
+        // GET: Marcas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var marcas = await _context.Marcas
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (marcas == null)
+            {
+                return NotFound();
+            }
+
+            return View(marcas);
+        }
+
+        // GET: Marcas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Marcas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre")] Marcas marcas)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(marcas);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(marcas);
+        }
+
+        // GET: Marcas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var marcas = await _context.Marcas.SingleOrDefaultAsync(m => m.Id == id);
+            if (marcas == null)
+            {
+                return NotFound();
+            }
+            return View(marcas);
+        }
+
+        // POST: Marcas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre")] Marcas marcas)
+        {
+            if (id != marcas.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(marcas);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MarcasExists(marcas.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(marcas);
+        }
+
+        // GET: Marcas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var marcas = await _context.Marcas
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (marcas == null)
+            {
+                return NotFound();
+            }
+
+            return View(marcas);
+        }
+
+        // POST: Marcas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var marcas = await _context.Marcas.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Marcas.Remove(marcas);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MarcasExists(int id)
+        {
+            return _context.Marcas.Any(e => e.Id == id);
+        }
+    }
+}
