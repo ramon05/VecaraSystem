b123e377dba57a1ebd2766bb2a150e1ccb44280f
ramon05 <ramoncastillo051991@gmail.com>
2019-11-18 22:21:22 -0400

    Se agregé el controller Pagos

---
+++ VecaraSystem/Controllers/PagosController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VecaraSystem.Data;
+using VecaraSystem.Models;
+
+namespace VecaraSystem.Controllers
+{
+    public class PagosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PagosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pagos
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Pagos.Include(p => p.ControlAccesoId).Include(p => p.TipoPagoId);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Pagos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pagos = await _context.Pagos
+                .Include(p => p.ControlAccesoId)
+                .Include(p => p.TipoPagoId)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (pagos == null)
+            {
+                return NotFound();
+            }
+
+            return View(pagos);
+        }
+
+        // GET: Pagos/Create
+        public IActionResult Create()
+        {
+            ViewData["IdControlAcceso"] = new SelectList(_context.ControlAcceso, "Id", "Id");
+            ViewData["IdTipoPago"] = new SelectList(_context.TipoPagos, "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Pagos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,IdTipoPago,IdControlAcceso,Monto,Fecha")] Pagos pagos)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pagos);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdControlAcceso"] = new SelectList(_context.ControlAcceso, "Id", "Id", pagos.IdControlAcceso);
+            ViewData["IdTipoPago"] = new SelectList(_context.TipoPagos, "Id", "Nombre", pagos.IdTipoPago);
+            return View(pagos);
+        }
+
+        // GET: Pagos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pagos = await _context.Pagos.SingleOrDefaultAsync(m => m.Id == id);
+            if (pagos == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdControlAcceso"] = new SelectList(_context.ControlAcceso, "Id", "Id", pagos.IdControlAcceso);
+            ViewData["IdTipoPago"] = new SelectList(_context.TipoPagos, "Id", "Nombre", pagos.IdTipoPago);
+            return View(pagos);
+        }
+
+        // POST: Pagos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,IdTipoPago,IdControlAcceso,Monto,Fecha")] Pagos pagos)
+        {
+            if (id != pagos.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pagos);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PagosExists(pagos.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdControlAcceso"] = new SelectList(_context.ControlAcceso, "Id", "Id", pagos.IdControlAcceso);
+            ViewData["IdTipoPago"] = new SelectList(_context.TipoPagos, "Id", "Nombre", pagos.IdTipoPago);
+            return View(pagos);
+        }
+
+        // GET: Pagos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pagos = await _context.Pagos
+                .Include(p => p.ControlAccesoId)
+                .Include(p => p.TipoPagoId)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (pagos == null)
+            {
+                return NotFound();
+            }
+
+            return View(pagos);
+        }
+
+        // POST: Pagos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pagos = await _context.Pagos.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Pagos.Remove(pagos);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PagosExists(int id)
+        {
+            return _context.Pagos.Any(e => e.Id == id);
+        }
+    }
+}
